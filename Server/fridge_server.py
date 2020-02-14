76c429897f99fd3ee951930f37f8f2017bf8e345
chradajan <chradajan@gmail.com>
2020-02-14 14:27:59 -0800

    Change server conditional

--- Server/fridge_server.py
+++ Server/fridge_server.py
@@ -67,7 +67,7 @@ class Fridge_Server:
         with open("../../snapshots/{}/snapshot.pgm".format(self.username), 'wb') as f:
             for _ in progress:
                 bytes_read = self.client.recv(4096)
-                if not bytes_read:
+                if len(bytes_read) < 5:
                     break
                 f.write(bytes_read)
                 progress.update(len(bytes_read))
